a95c6fff7c0193d157694d759e398e5d9df14ca8
mitchmi88 <mitchmi@web.de>
2026-06-04 09:16:20 +0200

    Upgrade Lenis to fix Chromium scroll lag

--- lib/lenis.ts
+++ lib/lenis.ts
@@ -1,9 +1,9 @@
-import Lenis from '@studio-freight/lenis'
+import Lenis from 'lenis'
 import { ScrollTrigger } from './gsap'
 
 export function initLenis() {
   const lenis = new Lenis({
-    duration: 1.4,
+    duration: 1.2,
     easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     smoothWheel: true,
   })
